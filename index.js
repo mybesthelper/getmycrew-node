e510e61215cc682583a8b522e6d69144440b12cd
marcin.popielarz@gmail.com <marcin.popielarz@gmail.com>
2015-09-18 11:12:25 -0700

    task create api call

--- index.js
+++ index.js
@@ -7,10 +7,22 @@ getMyCrew = new GetMyCrew({
 });
 
 getMyCrew.authenticate("marcin.popielarz@gmail.com", "marcin123", function() {
-  getMyCrew.index_tasks(function(data) {
-    console.log("task list");
+
+  // getMyCrew.index_tasks(function(data) {
+  //   console.log("task list");
+  //   console.log(data);
+  // });
+
+  getMyCrew.create_tasks({
+    title: 'My New Cool Task',
+    time: new Date(),
+    location: 'V6J1G1',
+    details: 'Some notes about task',
+    urgency: true,
+    contact_ids: [1,2,3]
+  }, function(data) {
+    console.log("task created");
     console.log(data);
   });
-
 })
 
