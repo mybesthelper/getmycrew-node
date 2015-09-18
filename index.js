a28f5f7df0c2d15a759cab82f5b891dae7a92f6b
marcin.popielarz@gmail.com <marcin.popielarz@gmail.com>
2015-09-18 15:42:30 -0700

    fetch api update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var GetMyCrew = require('./lib/getmycrew-node').GetMyCrew;
 
+
 getMyCrew = new GetMyCrew({
   client_id: '573464783de8e53595f554919556f698f9e63e9ea30c08bc225f520109e66dc6',
   client_secret: '664e5ca7b817c6eeb5892931e09745ff40005e3899ae9d1ab54300cbbe6bf036',
@@ -11,26 +12,26 @@ getMyCrew.authenticate("marcin.popielarz@gmail.com", "marcin123").then(function(
   console.log("authenticated");
   console.log(token);
 
-  getMyCrew.index_tasks().then(function(task_list) {
-    console.log("task list");
-    console.log(task_list);
-  });
+  // getMyCrew.index_tasks().then(function(data) {
+  //   console.log("task list");
+  //   console.log(data);
+  // });
 
-  getMyCrew.show_task(32).then(function(task) {
-    console.log("task shown");
-    console.log(task);
-  });
+  // getMyCrew.show_tasks(32).then(function(task) {
+  //   console.log("task shown");
+  //   console.log(task);
+  // });
 
-  getMyCrew.create_tasks({
-    title: 'My New Cool Task',
-    time: new Date(),
-    location: 'V6J1G1',
-    details: 'Some notes about task',
-    urgency: true,
-    contact_ids: [1,2,3]
-  }).then(function(task) {
-    console.log("task created");
-    console.log(task);
-  });
+  // getMyCrew.create_tasks({
+  //   title: 'My New Cool Task',
+  //   time: new Date(),
+  //   location: 'V6J1G1',
+  //   details: 'Some notes about task',
+  //   urgency: true,
+  //   contact_ids: [1,2,3]
+  // }).then(function(task) {
+  //   console.log("task created");
+  //   console.log(task);
+  // });
 
 });
